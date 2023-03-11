621f09d234388af9a6bd02bef07e1aad082b7875
nguyenthigai1905 <100947712+nguyenthigai1905@users.noreply.github.com>
2023-03-11 19:46:30 +0700

    Update preprocessing.py
    Add adaptive thresholding to image processing.
    Fix code based on feedback

--- core/preprocess/preprocessing.py
+++ core/preprocess/preprocessing.py
@@ -1,14 +1,10 @@
 from . import PREPROCESS
-import cv2
-from matplotlib import pyplot as plt
-import numpy as np
-import imutils
 
 @PREPROCESS.register()
 
 def grayscale(image: np.ndarray) -> np.ndarray:
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    return (image)
+    return image
 
 def noise_removal(image: np.ndarray) -> np.ndarray :
     kernel = np.ones((1, 1), np.uint8)
@@ -17,9 +13,11 @@ def noise_removal(image: np.ndarray) -> np.ndarray :
     image = cv2.erode(image, kernel, iterations=1)
     image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
     image = cv2.medianBlur(image, 3)
-    return (image)
+    return image
 
 def find_and_crop_contours(temp: np.ndarray, image: np.ndarray) -> np.ndarray:
+
+    temp = cv2.adaptiveThreshold(result,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV, 11, 5)
     
     # Find all contours in the image.
     contours, hierarchy = cv2.findContours(temp, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
