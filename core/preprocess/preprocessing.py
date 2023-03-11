be5348777edff591340d3a4c37f0aaa078cd71e3
nguyenthigai1905 <100947712+nguyenthigai1905@users.noreply.github.com>
2023-03-11 20:48:29 +0700

    Update preprocessing.py
    import lib
    add register

--- core/preprocess/preprocessing.py
+++ core/preprocess/preprocessing.py
@@ -1,11 +1,15 @@
 from . import PREPROCESS
 
-@PREPROCESS.register()
+import cv2
+import numpy as np 
 
+
+@PREPROCESS.register()
 def grayscale(image: np.ndarray) -> np.ndarray:
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     return image
 
+@PREPROCESS.register()
 def noise_removal(image: np.ndarray) -> np.ndarray :
     kernel = np.ones((1, 1), np.uint8)
     image = cv2.dilate(image, kernel, iterations=1)
@@ -15,6 +19,7 @@ def noise_removal(image: np.ndarray) -> np.ndarray :
     image = cv2.medianBlur(image, 3)
     return image
 
+@PREPROCESS.register()
 def find_and_crop_contours(temp: np.ndarray, image: np.ndarray) -> np.ndarray:
 
     temp = cv2.adaptiveThreshold(result,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV, 11, 5)
