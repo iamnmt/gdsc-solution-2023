d77a1c75cd0e819555d3874ec8d78e0c6524f1d9
Nguyen Thanh Tan <tan.personal.mail@gmail.com>
2023-03-18 14:31:19 +0700

    Add process with databaseof keyword

--- core/extractor/extractor.py
+++ core/extractor/extractor.py
@@ -1,6 +1,6 @@
 from keybert import KeyBERT
 
-doc = """
+doc = ["""
          Supervised learning is the machine learning task of learning a function that
          maps an input to an output based on example input-output pairs. It infers a
          function from labeled training data consisting of a set of training examples.
@@ -11,7 +11,15 @@ doc = """
          algorithm to correctly determine the class labels for unseen instances. This requires
          the learning algorithm to generalize from the training data to unseen situations in a
          'reasonable' way (see inductive bias).
+      """,
       """
+        Supervised learning, also known as supervised machine learning,
+        is a subcategory of machine learning and artificial intelligence.
+        It is defined by its use of labeled datasets to train algorithms that to classify data or predict outcomes accurately.
+        As input data is fed into the model, it adjusts its weights until the model has been fitted appropriately,
+        which occurs as part of the cross validation process. Supervised learning helps organizations solve for a variety of real-world problems at scale,
+        such as classifying spam in a separate folder from your inbox.
+      """]
 
 class Extractor:
     def __init__(self, keyphrase_ngram_range: tuple, threshold: float) -> None:
@@ -19,18 +27,31 @@ class Extractor:
         self.model = KeyBERT()
         self.threshold = threshold
         self.keywords = []
+        self.database = []
 
-    def _process(self, doc: str) -> None:
+    def _process(self, docs: list) -> None:
         '''
             (Private) Process keyword extraction
             Input: Document as string
             Output: None, store keywords in keywords attribute
         '''
-        keywords = self.model.extract_keywords(doc, keyphrase_ngram_range=(1, 3), stop_words='english')
-        self.keywords = [word for word, prob in keywords if prob > self.threshold][:5]
-        if len(self.keywords) == 0:
-            self.keywords = [word for word, prob in keywords][:2]
+        keywords_list = self.model.extract_keywords(docs, keyphrase_ngram_range=(1, 3), stop_words='english')
+        #i = 0 #debug
+        for keywords in keywords_list:
+            #print(f"Document #{i}: ")
+            self.keywords = [word for word, prob in keywords if prob > self.threshold][:5]
+            # Low diversity keyword handle
+            if len(self.keywords) < 2:
+                self.keywords = [word for word, prob in keywords][:2]
+            # Add known keywords in database
+            self.keywords += [word for word, prob in keywords if word in self.database]
+            #print(f"Keywords: {self.keywords}")
+            self.save_to_database()
+            #i += 1
         
+    def save_to_database(self) -> None:
+        self.database += [keyword for keyword in self.keywords if keyword not in self.database]
+
     def get_keywords(self, doc: str) -> list:
         '''
             Return keywords for after-processing
@@ -40,8 +61,6 @@ class Extractor:
         self._process(doc)
         return self.keywords
     
-ex = Extractor(keyphrase_ngram_range=(1, 3), threshold = 0.6)
+ex = Extractor(keyphrase_ngram_range=(1, 3), threshold=0.6)
 keywords = ex.get_keywords(doc=doc)
 print(keywords)
-    
-
